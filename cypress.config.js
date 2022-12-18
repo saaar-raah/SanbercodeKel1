485ef53fd95dd599843db5144e1c942c5b77c934
Pangestu Kurniawan <pangestuhuang87@gmail.com>
2022-12-18 22:46:06 +0700

    File Config

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = {
+  projectId: 'c1ey81',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
